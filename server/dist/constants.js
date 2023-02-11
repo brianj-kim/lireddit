12d777721bebad594c6b03aad3106867018e01a2
BrianJ Kim <brianjkim.dev@google.com>
2023-02-11 11:34:43 -0600

    Remove all the ignored directories

--- server/dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db_password = exports.db_username = exports.db_port = exports.db_host = exports.__prod__ = void 0;
-exports.__prod__ = process.env.NODE_ENV === 'production';
-exports.db_host = process.env.NODE_ENV === '127.0.0..1';
-exports.db_port = process.env.NODE_ENV === '5432';
-exports.db_username = process.env.NODE_ENV === 'brian27';
-exports.db_password = process.env.NODE_ENV === 'love5430';
-//# sourceMappingURL=constants.js.map
